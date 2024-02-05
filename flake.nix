9df4c1e64136f267cfb625b67d796c668af0cfb3
Ch4s3r <lamprecht.patrick1@gmail.com>
2024-02-05 20:16:13 +0100

    Move outlook_src to inputs

--- flake.nix
+++ flake.nix
@@ -1,42 +1,31 @@
 {
-  description = "Flake utils demo";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    outlook_src = { url = "https://go.microsoft.com/fwlink/p/?linkid=525137"; flake = false; };
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        outlook_src = pkgs.fetchurl {
-          url = "https://go.microsoft.com/fwlink/p/?linkid=525137";
-          sha256 = "sha256-NUCQo3BbHizAOuY41JASAsMZOwSEIIAjji6HRkjs4Xs=";
-          curlOptsList = [ "-L" ];
-        };
-      in
-      {
-        packages = rec {
-          outlook = pkgs.stdenv.mkDerivation {
-            name = "outlook.app";
-            src = outlook_src;
-            phases = [ "unpackPhase" "installPhase" ];
-            unpackPhase = ''
-              ${pkgs.xar}/bin/xar -xf $src
-              cat Microsoft_Outlook.pkg/Payload | gunzip -dc | ${pkgs.cpio}/bin/cpio -i
-            '';
-            installPhase = ''
-              mkdir -p $out/Applications
-              cp -r Microsoft\ Outlook.app $out/Applications/Microsoft\ Outlook.app
-            '';
-          };
-          outlookWrapper = pkgs.writeShellScriptBin "outlookWrapper" ''
-            open ${outlook}/Applications/Microsoft\ Outlook.app
+  outputs = { self, nixpkgs, outlook_src }:
+    let
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+    in
+    {
+      packages.aarch64-darwin = rec {
+        outlook = pkgs.stdenv.mkDerivation {
+          name = "outlook.app";
+          src = outlook_src;
+          phases = [ "unpackPhase" "installPhase" ];
+          unpackPhase = ''
+            ${pkgs.xar}/bin/xar -xf $src
+            cat Microsoft_Outlook.pkg/Payload | gunzip -dc | ${pkgs.cpio}/bin/cpio -i
+          '';
+          installPhase = ''
+            mkdir -p $out/Applications
+            cp -r Microsoft\ Outlook.app $out/Applications/Microsoft\ Outlook.app
           '';
         };
-        devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.ripgrep ];
-        };
-      }
-    );
+        outlookWrapper = pkgs.writeShellScriptBin "outlookWrapper" ''
+          open ${outlook}/Applications/Microsoft\ Outlook.app
+        '';
+        default = outlookWrapper;
+      };
+    };
 }
