9e707e349e8412fd3f2e70e6bbcde2c4232d07aa
Ch4s3r <lamprecht.patrick1@gmail.com>
2024-05-25 14:45:37 +0200

    Update Docker download URL and SHA256 checksum

--- docker-desktop/default.nix
+++ docker-desktop/default.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "docker-desktop";
   src = pkgs.fetchurl {
     url = "https://desktop.docker.com/mac/main/arm64/Docker.dmg";
-    sha256 = "0d5822i5k36ijpdks90fmv6lxi9dl58pgxw90pkfc9dbrv7706nm";
+    sha256 = "sha256-VY3+7nZGf6/ETv8cLBXmNUPRGjYyObPeAl6/ZE3V89Y=";
   };
   nativeBuildInputs = with pkgs; [ fd (callPackage ../7zz { }) ];
   unpackPhase = ''
