cdc36ddee7c561a7fceb41330e718b4fcf14e4f2
Ch4s3r <lamprecht.patrick1@gmail.com>
2024-05-25 15:28:37 +0200

    feat: Add bazecor package to the repository

---
+++ bazecor/default.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.stdenv.mkDerivation {
+  name = "bazecor";
+  src = pkgs.fetchurl {
+    url = "https://github.com/Dygmalab/Bazecor/releases/download/v1.3.11/Bazecor-1.3.11-arm64.dmg";
+    sha256 = "sha256-CKXN2AnLoilmMe8j2DMyBdlIqRiPyQ4ciOikBEYCgSQ=";
+  };
+  nativeBuildInputs = with pkgs; [ fd (callPackage ../7zz { }) ];
+  unpackPhase = ''
+    7zz x $src  || true
+  '';
+  installPhase = ''
+    mkdir -p $out/Applications
+    export APP_PATH=$(fd -t d ".app" -1)
+    cp -a "$APP_PATH" $out/Applications
+  '';
+}
