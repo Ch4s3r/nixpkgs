5a73e2a1fc89ead11c1205f8f2172c11fa184bb0
Patrick 'Lampe' Lamprecht <Ch4s3r@users.noreply.github.com>
2024-07-14 10:57:21 +0200

    Update default.nix

--- docker-desktop/default.nix
+++ docker-desktop/default.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "docker-desktop";
   src = pkgs.fetchurl {
     url = "https://desktop.docker.com/mac/main/arm64/Docker.dmg";
-    sha256 = "sha256-VY3+7nZGf6/ETv8cLBXmNUPRGjYyObPeAl6/ZE3V89Y=";
+    sha256 = "sha256-GuYg6Srhz4e2YHuGsRp5Khp6Tr/doWY8ubzo8nX0CxA=";
   };
   nativeBuildInputs = with pkgs; [ fd _7zz ];
   unpackPhase = ''
