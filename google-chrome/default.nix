8d6af42364a7d142cf9a7f6cbedac0d4b96305f0
Patrick 'Lampe' Lamprecht <Ch4s3r@users.noreply.github.com>
2024-07-14 10:41:20 +0200

    chore: update google chrome

--- google-chrome/default.nix
+++ google-chrome/default.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "google-chrome";
   src = pkgs.fetchurl {
     url = "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg";
-    sha256 = "0cngms4h624hxa73g81pf37fhiy9hv3rz35y878miz0bwz80djxw";
+    sha256 = "sha256-0ur5wFDJF6hE9WwoGX7rhMuRfyIX1k3w54sXrg1ezIg=";
   };
   nativeBuildInputs = with pkgs; [ fd _7zz ];
   unpackPhase = ''
